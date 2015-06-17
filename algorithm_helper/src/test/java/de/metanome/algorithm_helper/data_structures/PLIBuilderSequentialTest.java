e3f8ce02504ad467c463660d184117b736ebce30
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2015-06-17 11:59:35 +0200

    implemented a sequential pli builder

---
+++ algorithm_helper/src/test/java/de/metanome/algorithm_helper/data_structures/PLIBuilderSequentialTest.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright 2015 by the Metanome project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.metanome.algorithm_helper.data_structures;
+
+import de.metanome.algorithm_integration.input.InputGenerationException;
+import de.metanome.algorithm_integration.input.InputIterationException;
+
+import org.hamcrest.collection.IsIterableContainingInAnyOrder;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertThat;
+
+/**
+ * Tests for {@link PLIBuilderSequential}
+ *
+ * @author Jakob Zwiener
+ */
+public class PLIBuilderSequentialTest {
+
+  protected PLIBuilderFixture fixture;
+  protected PLIBuilderSequential builder;
+
+  @Before
+  public void setUp() throws Exception {
+    fixture = new PLIBuilderFixture();
+    builder = new PLIBuilderSequential(fixture.getInputGenerator());
+  }
+
+  /**
+   * Test method for {@link PLIBuilderSequential#getPLIList()} <p/> Tests that {@link
+   * de.metanome.algorithm_helper.data_structures.PositionListIndex}es are build correctly.
+   */
+  @Test
+  public void testCalculatePLINullEqualsNull() throws PLIBuildingException {
+    // Setup
+    // Expected values
+    List<PositionListIndex> expectedPLIList = fixture.getExpectedPLIList(true);
+    PositionListIndex[]
+        expectedPLIArray =
+        expectedPLIList.toArray(new PositionListIndex[expectedPLIList.size()]);
+
+    // Execute functionality
+    List<PositionListIndex> actualPLIList = builder.getPLIList();
+
+    // Check result
+    assertThat(actualPLIList, IsIterableContainingInAnyOrder.containsInAnyOrder(expectedPLIArray));
+  }
+
+  /**
+   * Test method for {@link PLIBuilderSequential#getPLIList()} <p/> Tests that {@link
+   * de.metanome.algorithm_helper.data_structures.PositionListIndex}es are build correctly.
+   */
+  @Test
+  public void testCalculatePLINullNotEqualsNull()
+      throws PLIBuildingException, InputGenerationException, InputIterationException {
+    // Setup
+    this.builder = new PLIBuilderSequential(fixture.getInputGenerator(), false);
+    // Expected values
+    List<PositionListIndex> expectedPLIList = fixture.getExpectedPLIList(false);
+    PositionListIndex[]
+        expectedPLIArray =
+        expectedPLIList.toArray(new PositionListIndex[expectedPLIList.size()]);
+
+    // Execute functionality
+    List<PositionListIndex> actualPLIList = builder.getPLIList();
+
+    // Check result
+    assertThat(actualPLIList, IsIterableContainingInAnyOrder.containsInAnyOrder(expectedPLIArray));
+  }
+}
