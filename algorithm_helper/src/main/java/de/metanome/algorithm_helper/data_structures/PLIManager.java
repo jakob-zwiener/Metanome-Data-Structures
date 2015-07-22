0948202076df1c346cb6b2d6ae8c63e532c9da5d
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2015-07-22 13:52:00 +0200

    calculating set cover good speedup for ducc

--- algorithm_helper/src/main/java/de/metanome/algorithm_helper/data_structures/PLIManager.java
+++ algorithm_helper/src/main/java/de/metanome/algorithm_helper/data_structures/PLIManager.java
@@ -16,6 +16,7 @@
 
 package de.metanome.algorithm_helper.data_structures;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -26,7 +27,9 @@ import java.util.Map;
  */
 public class PLIManager {
 
-  public long searchTime;
+  public long searchTimes;
+  public long intersectTimes;
+  public int numberOfIntersects;
   protected Map<ColumnCombinationBitset, PositionListIndex> plis;
   protected ColumnCombinationBitset allColumnCombination;
   protected SubSetGraph pliGraph;
@@ -48,9 +51,70 @@ public class PLIManager {
         "The column combination should only contain column indices of plis that are known to the pli manager.");
     }
 
+    PositionListIndex exactPli = plis.get(columnCombination);
+    if (exactPli != null) {
+      return exactPli;
+    }
 
-    long startSearch = System.nanoTime();
     List<ColumnCombinationBitset> subsets = pliGraph.getExistingSubsets(columnCombination);
+
+    System.out.println(subsets.size());
+
+    ColumnCombinationBitset unionSoFar = new ColumnCombinationBitset();
+    ColumnCombinationBitset bestSubset = new ColumnCombinationBitset();
+    ColumnCombinationBitset bestUnion = new ColumnCombinationBitset();
+    List<ColumnCombinationBitset> solution = new LinkedList<>();
+    while (!unionSoFar.equals(columnCombination)) {
+      for (ColumnCombinationBitset subset : subsets) {
+
+        ColumnCombinationBitset currentUnion = unionSoFar.union(subset);
+
+        if (currentUnion.size() > bestUnion.size()) {
+          bestSubset = subset;
+          bestUnion = currentUnion;
+        }
+      }
+      solution.add(bestSubset);
+      unionSoFar = unionSoFar.union(bestSubset);
+    }
+
+    System.out.println(solution.size());
+
+    long beforeIntersect = System.nanoTime();
+
+    PositionListIndex intersect = null;
+    ColumnCombinationBitset unionCombination = null;
+    for (ColumnCombinationBitset subsetCombination : solution) {
+      if (intersect == null) {
+        intersect = plis.get(subsetCombination);
+        unionCombination = subsetCombination;
+        continue;
+      }
+
+      intersect = intersect.intersect(plis.get(subsetCombination));
+      unionCombination = unionCombination.union(subsetCombination);
+      plis.put(unionCombination, intersect);
+      pliGraph.add(unionCombination);
+    }
+
+    long intersectTime = System.nanoTime() - beforeIntersect;
+    this.intersectTimes += intersectTime;
+    System.out.println(String.format("%fs intersect time", intersectTimes / 1000000000d));
+    System.out.println(String.format("%d intersects", numberOfIntersects++));
+
+
+/*
+      ColumnCombinationBitset difference = columnCombination.minus(subset);
+      PositionListIndex differencePli = plis.get(difference);
+      if (differencePli != null) {
+        System.out.println("found--------");
+        return differencePli.intersect(plis.get(subset));
+      }
+    }*/
+
+
+    /*long startSearch = System.nanoTime();
+
     PositionListIndex minPli = null;
     int minKeyError = Integer.MAX_VALUE;
     // FIXME(zwiener): A null pointer exception could originate here. Add test for the case that no subsets are found.
@@ -60,22 +124,33 @@ public class PLIManager {
       if (pli.getRawKeyError() < minKeyError) {
         minPli = pli;
         minKeyErrorSubset = subset;
+        minKeyError = pli.getRawKeyError();
       }
     }
-    searchTime += (System.nanoTime() - startSearch);
+    searchTimes += (System.nanoTime() - startSearch);
+
+    long beforeIntersect = System.nanoTime();
 
     ColumnCombinationBitset missingColumns = columnCombination.minus(minKeyErrorSubset);
 
     PositionListIndex intersect = minPli;
     ColumnCombinationBitset currentColumnCombination = minKeyErrorSubset;
     System.out.println(missingColumns);
+    int intersects = 0;
     for (ColumnCombinationBitset oneColumnCombination : missingColumns.getContainedOneColumnCombinations()) {
       intersect = intersect.intersect(plis.get(oneColumnCombination));
+      intersects++;
       // TODO(zwiener): Whether plis get cached or not is currently not tested.
       currentColumnCombination = currentColumnCombination.union(oneColumnCombination);
       plis.put(currentColumnCombination, intersect);
       pliGraph.add(currentColumnCombination);
     }
+
+    long intersectTime = System.nanoTime() - beforeIntersect;
+    this.intersectTimes += intersectTime;
+    System.out.println(String.format("%d intra intersects", intersects));
+    System.out.println(String.format("%fs intersect time", intersectTimes / 1000000000d));
+    System.out.println(String.format("%d intersects", numberOfIntersects++));*/
 
     return intersect;
   }
