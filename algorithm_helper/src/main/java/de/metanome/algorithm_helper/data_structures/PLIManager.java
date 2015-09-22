6ff7a14eb1a942b8f5ea4718c25141c15ab51f15
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2015-09-22 13:36:47 +0200

    implemented multi intersect ordering without subset graph

--- algorithm_helper/src/main/java/de/metanome/algorithm_helper/data_structures/PLIManager.java
+++ algorithm_helper/src/main/java/de/metanome/algorithm_helper/data_structures/PLIManager.java
@@ -17,7 +17,6 @@
 package de.metanome.algorithm_helper.data_structures;
 
 import java.util.Comparator;
-import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 
@@ -28,17 +27,11 @@ import java.util.PriorityQueue;
  */
 public class PLIManager {
 
-  public long searchTimes;
-  public long intersectTimes;
-  public int numberOfIntersects;
   protected Map<ColumnCombinationBitset, PositionListIndex> plis;
   protected ColumnCombinationBitset allColumnCombination;
-  protected SubSetGraph pliGraph;
 
   public PLIManager(final Map<ColumnCombinationBitset, PositionListIndex> plis) {
     this.plis = plis;
-    pliGraph = new SubSetGraph();
-    pliGraph.addAll(plis.keySet());
     int[] allColumnIndices = new int[plis.size()];
     for (int i = 0; i < plis.size(); i++) {
       allColumnIndices[i] = i;
@@ -57,19 +50,15 @@ public class PLIManager {
       return exactPli;
     }
 
-    List<ColumnCombinationBitset> subsets = pliGraph.getExistingSubsets(columnCombination);
-
     PriorityQueue<ColumnCombinationBitset> pq = new PriorityQueue<ColumnCombinationBitset>(
-      subsets.size(),
+      columnCombination.size(),
       new Comparator<ColumnCombinationBitset>() {
         @Override public int compare(final ColumnCombinationBitset o1, final ColumnCombinationBitset o2) {
           return plis.get(o1).getRawKeyError() - plis.get(o2).getRawKeyError();
         }
       });
 
-    pq.addAll(subsets);
-
-    long beforeIntersect = System.nanoTime();
+    pq.addAll(columnCombination.getContainedOneColumnCombinations());
 
     ColumnCombinationBitset unionSoFar = pq.poll();
     PositionListIndex intersect = plis.get(unionSoFar);
@@ -83,115 +72,6 @@ public class PLIManager {
       unionSoFar = unionSoFar.union(currentSubset);
       intersect = intersect.intersect(plis.get(currentSubset));
     }
-
-    long intersectTime = System.nanoTime() - beforeIntersect;
-    this.intersectTimes += intersectTime;
-    System.out.println(String.format("%fs intersect time", intersectTimes / 1000000000d));
-    System.out.println(String.format("%d intersects", numberOfIntersects++));
-
-
-    /*System.out.println(subsets.size());
-
-    ColumnCombinationBitset unionSoFar = new ColumnCombinationBitset();
-    ColumnCombinationBitset bestSubset = new ColumnCombinationBitset();
-    ColumnCombinationBitset bestUnion = new ColumnCombinationBitset();
-    List<ColumnCombinationBitset> solution = new LinkedList<>();
-    while (!unionSoFar.equals(columnCombination)) {
-      for (ColumnCombinationBitset subset : subsets) {
-
-        ColumnCombinationBitset currentUnion = unionSoFar.union(subset);
-
-        if (currentUnion.size() > bestUnion.size()) {
-          bestSubset = subset;
-          bestUnion = currentUnion;
-        }
-        else if (currentUnion.size() == bestUnion.size()) {
-          System.out.println("equal--------");
-          System.out.println(String.format("best: %d current: %d", plis.get(bestSubset).getRawKeyError(), plis.get(
-            subset).getRawKeyError()));
-          if (plis.get(bestSubset).getRawKeyError() > plis.get(subset).getRawKeyError()) {
-            bestSubset = subset;
-            bestUnion = currentUnion;
-          }
-        }
-      }
-      solution.add(bestSubset);
-      unionSoFar = unionSoFar.union(bestSubset);
-    }
-
-    System.out.println(solution.size());
-
-    long beforeIntersect = System.nanoTime();
-
-    PositionListIndex intersect = null;
-    ColumnCombinationBitset unionCombination = null;
-    for (ColumnCombinationBitset subsetCombination : solution) {
-      if (intersect == null) {
-        intersect = plis.get(subsetCombination);
-        unionCombination = subsetCombination;
-        continue;
-      }
-
-      intersect = intersect.intersect(plis.get(subsetCombination));
-      unionCombination = unionCombination.union(subsetCombination);
-      plis.put(unionCombination, intersect);
-      pliGraph.add(unionCombination);
-    }
-
-    long intersectTime = System.nanoTime() - beforeIntersect;
-    this.intersectTimes += intersectTime;
-    System.out.println(String.format("%fs intersect time", intersectTimes / 1000000000d));
-    System.out.println(String.format("%d intersects", numberOfIntersects++));*/
-
-
-/*
-      ColumnCombinationBitset difference = columnCombination.minus(subset);
-      PositionListIndex differencePli = plis.get(difference);
-      if (differencePli != null) {
-        System.out.println("found--------");
-        return differencePli.intersect(plis.get(subset));
-      }
-    }*/
-
-
-    /*long startSearch = System.nanoTime();
-
-    PositionListIndex minPli = null;
-    int minKeyError = Integer.MAX_VALUE;
-    // FIXME(zwiener): A null pointer exception could originate here. Add test for the case that no subsets are found.
-    ColumnCombinationBitset minKeyErrorSubset = null;
-    for (ColumnCombinationBitset subset : subsets) {
-      PositionListIndex pli = plis.get(subset);
-      if (pli.getRawKeyError() < minKeyError) {
-        minPli = pli;
-        minKeyErrorSubset = subset;
-        minKeyError = pli.getRawKeyError();
-      }
-    }
-    searchTimes += (System.nanoTime() - startSearch);
-
-    long beforeIntersect = System.nanoTime();
-
-    ColumnCombinationBitset missingColumns = columnCombination.minus(minKeyErrorSubset);
-
-    PositionListIndex intersect = minPli;
-    ColumnCombinationBitset currentColumnCombination = minKeyErrorSubset;
-    System.out.println(missingColumns);
-    int intersects = 0;
-    for (ColumnCombinationBitset oneColumnCombination : missingColumns.getContainedOneColumnCombinations()) {
-      intersect = intersect.intersect(plis.get(oneColumnCombination));
-      intersects++;
-      // TODO(zwiener): Whether plis get cached or not is currently not tested.
-      currentColumnCombination = currentColumnCombination.union(oneColumnCombination);
-      plis.put(currentColumnCombination, intersect);
-      pliGraph.add(currentColumnCombination);
-    }
-
-    long intersectTime = System.nanoTime() - beforeIntersect;
-    this.intersectTimes += intersectTime;
-    System.out.println(String.format("%d intra intersects", intersects));
-    System.out.println(String.format("%fs intersect time", intersectTimes / 1000000000d));
-    System.out.println(String.format("%d intersects", numberOfIntersects++));*/
 
     return intersect;
   }
