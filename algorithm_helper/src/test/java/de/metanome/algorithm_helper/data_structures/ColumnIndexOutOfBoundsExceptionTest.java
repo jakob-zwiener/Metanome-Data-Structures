44d0f5e8cfa8895e8e50a027f7b4a216d955f058
Jakob Zwiener <jakob.zwiener@student.hpi.uni-potsdam.de>
2015-09-23 23:30:16 +0200

    added a ColumnIndexOutOfBoundsException.java

---
+++ algorithm_helper/src/test/java/de/metanome/algorithm_helper/data_structures/ColumnIndexOutOfBoundsExceptionTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright 2015 by the Metanome project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.metanome.algorithm_helper.data_structures;
+
+import org.junit.Test;
+
+import java.io.FileNotFoundException;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Tests for {@link ColumnIndexOutOfBoundsException}
+ *
+ * @author Jakob Zwiener
+ */
+public class ColumnIndexOutOfBoundsExceptionTest {
+
+  /**
+   * Test for {@link ColumnIndexOutOfBoundsException#ColumnIndexOutOfBoundsException(String)} <p>
+   * The exception should store the message.
+   */
+  @Test
+  public void testColumnIndexOutOfBoundsExceptionString() {
+    // Setup
+    // Expected values
+    String expectedMessage = "some message";
+
+    // Execute functionality
+    String actualMessage;
+    try {
+      throw new ColumnIndexOutOfBoundsException(expectedMessage);
+    } catch (ColumnIndexOutOfBoundsException e) {
+      actualMessage = e.getMessage();
+    }
+
+    // Check result
+    assertEquals(expectedMessage, actualMessage);
+  }
+
+  /**
+   * Test method for {@link ColumnIndexOutOfBoundsException#ColumnIndexOutOfBoundsException(String,
+   * Throwable)} <p> The exception should store the message and the cause.
+   */
+  @Test
+  public void testColumnIndexOutOfBoundsExceptionStringThrowable() {
+    // Setup
+    // Expected values
+    String expectedMessage = "some message";
+    Throwable expectedCause = new FileNotFoundException("some file was not found");
+
+    // Execute functionality
+    String actualMessage;
+    Throwable actualCause;
+    try {
+      throw new ColumnIndexOutOfBoundsException(expectedMessage, expectedCause);
+    } catch (ColumnIndexOutOfBoundsException e) {
+      actualMessage = e.getMessage();
+      actualCause = e.getCause();
+    }
+
+    // Check result
+    assertEquals(expectedMessage, actualMessage);
+    assertEquals(expectedCause, actualCause);
+  }
+
+}
